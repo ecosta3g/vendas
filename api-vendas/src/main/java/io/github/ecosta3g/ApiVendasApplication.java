e601370d5e3e916a052474ffbf8fe923ecd72828
Eduardo Costa <ecosta@info.ufrn.br>
2020-07-11 16:43:57 -0300

    Configuração externalizada com

--- api-vendas/src/main/java/io/github/ecosta3g/ApiVendasApplication.java
+++ api-vendas/src/main/java/io/github/ecosta3g/ApiVendasApplication.java
@@ -1,6 +1,7 @@
 package io.github.ecosta3g;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,14 +16,22 @@ public class ApiVendasApplication {
 	@Autowired
 	private MinhaConfiguration minhaConfiguration;
 	
+	@Value("${spring.application.name}")
+	private String applicationName;
+	
 	@GetMapping("/hello")
 	public String helloWorld() {
 		return "Hello World";
 	}
-	
-	@GetMapping("/nomeAplicacao")
+
+	@GetMapping("/v1/nomeAplicacao")
 	public String nomeAplicacao() {
 		return minhaConfiguration.nomeAplicacao();
+	}
+	
+	@GetMapping("/v2/nomeAplicacao")
+	public String nomeAplicacaoAP() {
+		return applicationName;
 	}
 
 	public static void main(String[] args) {
