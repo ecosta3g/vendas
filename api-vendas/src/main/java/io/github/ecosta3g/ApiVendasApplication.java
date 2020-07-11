b93f70d49637af435bfe64a641609ffa200a0005
Eduardo Costa <ecosta@info.ufrn.br>
2020-07-11 15:15:24 -0300

    Iniciando o projeto da API

---
+++ api-vendas/src/main/java/io/github/ecosta3g/ApiVendasApplication.java
@@ -0,0 +1,13 @@
+package io.github.ecosta3g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVendasApplication.class, args);
+	}
+
+}
---
+++ api-vendas/src/test/java/io/github/ecosta3g/ApiVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ecosta3g;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
