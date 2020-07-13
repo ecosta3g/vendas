115d60a62e9ebe5c991296b0d07d0ba60c3c1510
Eduardo Costa <ecosta@info.ufrn.br>
2020-07-13 14:14:06 -0300

    Obtendo informações do cliente

--- api-vendas/src/main/java/io/github/ecosta3g/rest/ClienteController.java
+++ api-vendas/src/main/java/io/github/ecosta3g/rest/ClienteController.java
@@ -1,13 +1,17 @@
 package io.github.ecosta3g.rest;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import io.github.ecosta3g.model.entity.Cliente;
 import io.github.ecosta3g.model.repository.ClienteRepository;
@@ -23,6 +27,11 @@ public class ClienteController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente salvar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente buscarPorId(@PathVariable Integer id) {
+		return clienteRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 
 }
