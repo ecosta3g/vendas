3feca816e09b76e2e7571f4127671bbf407efcf9
Eduardo Costa <ecosta@info.ufrn.br>
2020-07-13 13:59:47 -0300

    Cadastro de ceinte através do Postman

--- api-vendas/src/main/java/io/github/ecosta3g/ApiVendasApplication.java
+++ api-vendas/src/main/java/io/github/ecosta3g/ApiVendasApplication.java
@@ -2,6 +2,8 @@ package io.github.ecosta3g;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ApiVendasApplication {
@@ -9,5 +11,4 @@ public class ApiVendasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiVendasApplication.class, args);
 	}
-
 }
