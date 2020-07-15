a75d1fecf958679ac17dfd154e41c59c3406dabd
Eduardo Costa <ecosta@info.ufrn.br>
2020-07-15 13:34:53 -0300

    Componente Hello World

--- app-vendas/src/app/app.module.ts
+++ app-vendas/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HelloComponent } from './hello/hello.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,7 +7,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
     BrowserModule,
