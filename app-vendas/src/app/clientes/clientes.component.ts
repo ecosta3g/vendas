03f38c939763dd8c69332615a97b32645584b225
Eduardo Costa <ecosta@info.ufrn.br>
2020-07-15 15:48:47 -0300

    Templates e interpolação

--- app-vendas/src/app/clientes/clientes.component.ts
+++ app-vendas/src/app/clientes/clientes.component.ts
@@ -5,4 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './clientes.component.html',
   styleUrls: ['./clientes.component.css']
 })
-export class ClientesComponent {}
+export class ClientesComponent {
+  nome: string;
+  clientes: Cliente[];
+
+  constructor(){
+    this.nome = 'Edu';
+    const fulano = new Cliente('Fulano', 10);
+    const cicrano = new Cliente('Cicrano', 11);
+
+    this.clientes = [fulano, cicrano];
+  }
+
+}
+
+class Cliente {
+  constructor(
+    public nome: string,
+    public idade: number
+  ){
+
+  }
+}
