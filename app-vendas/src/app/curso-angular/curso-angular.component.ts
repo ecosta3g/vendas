29368f66583d49f6381b707929f5f5c92a3c6904
Eduardo Costa <ecosta@info.ufrn.br>
2020-07-15 13:46:10 -0300

    Criando componentes através do Angular CLI

---
+++ app-vendas/src/app/curso-angular/curso-angular.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curso-angular',
+  templateUrl: './curso-angular.component.html',
+  styleUrls: ['./curso-angular.component.css']
+})
+export class CursoAngularComponent {}
